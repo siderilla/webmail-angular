215ee7ba5f54b593f00c0452412ff42aa49d0a23
siderilla <siderilla11@gmail.com>
2025-09-15 12:43:55 +0200

    ok

--- src/app/mail-card/mail-card.component.ts
+++ src/app/mail-card/mail-card.component.ts
@@ -1,14 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type MessageDetail } from '../model/message-detail';
+import { DatePipe } from '@angular/common';
 
 @Component({
 	selector: 'app-mail-card',
-	imports: [],
+	imports: [DatePipe],
 	templateUrl: './mail-card.component.html',
 	styleUrl: './mail-card.component.scss'
 })
 export class MailCardComponent {
 
 	@Input() message?: MessageDetail;
+	@Output() onMessageSelected = new EventEmitter<{ message: MessageDetail, selected: boolean }>();
 
 }
--- src/app/mail-viewer/mail-viewer.component.ts
+++ src/app/mail-viewer/mail-viewer.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { MailTextComponent } from '../mail-text/mail-text.component';
 import { MailListComponent } from '../mail-list/mail-list.component';
+import { type MessageDetail } from '../model/message-detail';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-mail-viewer',
-  imports: [MailTextComponent, MailListComponent],
-  templateUrl: './mail-viewer.component.html',
-  styleUrl: './mail-viewer.component.scss'
+	selector: 'app-mail-viewer',
+	imports: [MailTextComponent, MailListComponent, CommonModule],
+	templateUrl: './mail-viewer.component.html',
+	styleUrl: './mail-viewer.component.scss'
 })
 export class MailViewerComponent {
 
+	messages: MessageDetail[] = [
+		{
+			date: new Date("2025-09-11"),
+			from: "alice@example.com",
+			subject: "Caffè?",
+			body: "Ehi Bob, ci prendiamo un caffè più tardi?"
+		},
+		{
+			date: new Date("2025-09-13"),
+			from: "carol@example.com",
+			subject: "Meeting aggiornato",
+			body: "Il meeting è spostato alle 11:00 in sala A."
+		},
+		{
+			date: new Date("2025-09-12"),
+			from: "dave@example.com",
+			subject: "Pranzo veloce",
+			body: "Ti va un panino veloce al bar sotto?"
+		}
+	];
 }
