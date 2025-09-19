9b79f04416c0983d5bdae0f10c64403900e68807
siderilla <siderilla11@gmail.com>
2025-09-19 17:47:54 +0200

    aggiustata la view, service per la get

--- src/app/services/mail.service.ts
+++ src/app/services/mail.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { type Mail } from '../model/mail';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MailService {
+
+	private http = inject(HttpClient);
+	private apiUrl = '/assets/mails.json';
+
+	getAllMails(): Observable<Mail[]> {
+		return this.http.get<Mail[]>(this.apiUrl).pipe(
+			map(mails => mails.map)
+		)
+	}
   
 }
