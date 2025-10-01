d48747eb6a419578b33730f7c67bb113666171fb
siderilla <siderilla11@gmail.com>
2025-10-01 11:07:26 +0200

    mobile compose actions redirect to home

--- src/app/services/composeActions.service.ts
+++ src/app/services/composeActions.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { ModalService } from './modal.service';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Injectable({
 	providedIn: 'root'
@@ -10,26 +11,39 @@ export class ComposeActionService {
 
 	private router = inject(Router);
 	private modal = inject(ModalService);
+	private breakpointObserver = inject(BreakpointObserver);
 
-	private modalSubject = new BehaviorSubject<boolean>(false);
-	isModalVisible$ = this.modalSubject.asObservable();
+	isMobile: boolean = false;
+
+	constructor() {
+		this.breakpointObserver
+		.observe('(max-width: 767px)')
+		.subscribe(state => {
+			this.isMobile = state.matches;
+		});
+	}
 
 	send() {
 		console.log("Mail sent");
-		if (this.isModalVisible$) {
-			this.modal.closeModal();
-		} else {
-			this.router.navigate(['/']);
+		this.modal.closeModal();
+		if (this.isMobile) {
+			this.router.navigate(['']);
 		}
 	}
 
 	draft() {
 		console.log("Draft saved");
 		this.modal.closeModal();
+		if (this.isMobile) {
+			this.router.navigate(['']);
+		}
 	}
 
 	discard() {
 		console.log("Mail discarded");
 		this.modal.closeModal();
+		if (this.isMobile) {
+			this.router.navigate(['']);
+		}
 	}
 }
